621c41b57b66d117af51736e4dcc8b1d995ce37b
George Henrique <georgehenriquecampos@gmail.com>
2021-11-08 21:35:38 -0300

    Footer fixed.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,8 +6,10 @@ const Footer: React.FC= () => {
         <>
             <Rodape>
                 <footer>
-                    <p>&#169; Portfolio-george-henrique.netlify.app 2021</p>
-                    <p>Feito com <strong className="heart">&#9829;</strong> <strong className="br">🇧🇷</strong></p>
+                    <div className="div-footer">
+                        <p>&#169; Portfolio-george-henrique.netlify.app 2021</p>
+                        <p>Feito com <strong className="heart">&#9829;</strong> <strong className="br">🇧🇷</strong></p>
+                    </div>
                 </footer>
             </Rodape>
         </>
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -2,14 +2,22 @@ import styled from "styled-components";
 
 const Rodape = styled.footer `
     footer {
+        display: flex;
+        justify-content: center;
         position: static;
         bottom: 0;
         left: 0;
-        width: 100%;
+        min-width: 100%;
+        text-align: center;
+        padding: 4.4px;
         background-color: rgba(60,56,54, 0.7);
     }
+    div.div-footer {
+        margin-left: 15%;
+        width: 50%;
+    }
     p {
-        line-height: 1.5pc;
+        line-height: 1.3pc;
     }
     .heart {
         color: #dc143c;
